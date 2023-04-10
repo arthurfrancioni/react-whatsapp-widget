ec0e00cf05f89bc39b04a39f0bd961df2e87a3a0
arthur <arthurfrancioni@gmail.com>
2023-04-10 11:38:45 -0300

    update react and fix problems

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import App from './components/App'
 import { WidgetContextProvider } from './context/widgetContext'
 
 export default function WhatsAppWidget({ chat }) {
-  const DEFAULT = {
-    name: 'Arthur',
-    status: 'Online',
-    message: 'Hello! 👋🏼 <br><br> What can we do for you?',
-    button: 'Start Chat',
-    initTime: 2000,
-    buttonStyle: 'white',
-    avatar: null,
-    phoneNumber: null
-  }
+  const [setDefaultConfig, defaultConfig] = useState(null)
+
+  useEffect(() => {
+    setDefaultConfig({
+      name: 'Arthur',
+      status: 'Online',
+      message: 'Hello! 👋🏼 <br><br> What can we do for you?',
+      button: 'Start Chat',
+      initTime: 2000,
+      buttonStyle: 'white',
+      avatar: null,
+      phoneNumber: null
+    })
+  }, [])
+
   return (
     <WidgetContextProvider>
-      <App {...(chat || DEFAULT)} />
+      <App {...(chat || defaultConfig)} />
     </WidgetContextProvider>
   )
 }
